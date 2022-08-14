0b6e549e1cf6ced213f91cb9f08bfa480e87d322
PdskDev <prestadesk@gmail.com>
2022-08-14 12:52:51 +0200

    Sprint boot - Security - Signup/Update - Password encryption

--- SpringBootSample/src/main/java/com/nadet/demo/domain/user/service/impl/UserServiceImpl.java
+++ SpringBootSample/src/main/java/com/nadet/demo/domain/user/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.nadet.demo.domain.user.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,11 +17,19 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserMapper mapper;
 	
+	@Autowired
+	private PasswordEncoder encoder;
+	
 	/** User signup **/
 	@Override
 	public void signup(MUser user) {
 		user.setDepartmentId(1);
 		user.setRole("ROLE_GENERAL");
+		
+		//Password encryption
+		String rawPassword = user.getPassword();
+		user.setPassword(encoder.encode(rawPassword));
+		
 		mapper.insertOne(user);
 	}
 	
@@ -41,7 +50,11 @@ public class UserServiceImpl implements UserService {
 	@Transactional
 	@Override
 	public void updateUserOne(String userId, String password, String userName) {
-		 mapper.updateOne(userId, password, userName);
+		
+		//Password encryption
+		String encryptPassword = encoder.encode(password);
+		
+		mapper.updateOne(userId, encryptPassword, userName);
 	}
 	
 	/** Delete user **/
